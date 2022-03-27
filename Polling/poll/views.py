e7d6e58c30d27ac3db59f8813e6dde2744d050cc
royabhi00 <abhishekanand20001027@gmail.com>
2022-03-27 15:38:45 +0530

    pollingApp

---
+++ Polling/poll/views.py
@@ -0,0 +1,85 @@
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404, HttpResponseRedirect, HttpResponse
+from django.urls import reverse
+from django.template import loader
+from .models import Question, Choice
+
+# Create your views here.
+def index(request):
+    question_list = Question.objects.all()
+    """
+    output = ', '.join([q.question_text for q in question_list])
+    return HttpResponse(output)
+    """
+    """
+     ALSO A WAY TO RENDER HTML PAGE
+    template = loader.get_template('poll/index.html')
+    context = {
+        'question_list': question_list,
+    }
+    return HttpResponse(template.render(context, request))
+    """
+    context = {
+        'question_list': question_list,
+    }
+    return render(request, 'poll/index.html', context)
+
+def detail(request, question_id):
+    # USING HTTP404
+    """
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'poll/detail.html', {'question': question})
+    """
+    # USING GET_OBJECT_OR_404
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'poll/detail.html', {'question': question})
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'poll/results.html', {'question': question})
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError,  Choice.DoesNotExist): 
+        return render(request, 'poll/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('poll:results', args=(question.id,)))
+
+
+# FOR CLASS VIEWS
+"""
+from django.views import generic
+
+
+class IndexView(generic.ListView):
+    template_name = 'poll/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        # Return the last five published questions.
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
+
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'poll/detail.html'
+
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'poll/results.html'
+"""
